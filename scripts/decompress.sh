0725169ec5b1952fc3c9337fcef8dad783f32baf
Manuel Chonon <machfe14@gmail.com>
2025-04-13 16:19:26 +0100

    Fixing issue with pipeline

--- scripts/decompress.sh
+++ scripts/decompress.sh
@@ -4,6 +4,7 @@ if [ -e "build/files" ]; then
     echo "Files already decompressed"
 else
     echo "Decompressing files"
+	mkdir -p build
 	cat static/files.part_* > build/files.gz
 	tar -xzf build/files.gz -C build/
 fi
